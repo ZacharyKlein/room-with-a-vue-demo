4bb248b999131665e97b1b36e49f8b58af966adc
Zachary Klein <zak@silver-chalice.com>
2017-12-06 15:15:54 -0500

    Added hover effect to Book

--- client/src/components/Book.vue
+++ client/src/components/Book.vue
@@ -1,5 +1,7 @@
 <template>
-    <tr class="book-row">
+    <tr  v-bind:class="{ hover: hover, 'book-row': true }"
+         @mouseover="hover = true"
+         @mouseleave="hover = false">
       <td class="title">{{book.title}}</td>
       <td class="pages">{{book.pages}}</td>
       <td class="author">{{book.author}}</td>
