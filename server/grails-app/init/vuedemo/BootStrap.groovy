91c37afe6f82877fb649d392250b9431a234a33f
Zachary Klein <zak@silver-chalice.com>
2017-12-06 12:26:59 -0500

    added domain model, data services

--- server/grails-app/init/vuedemo/BootStrap.groovy
+++ server/grails-app/init/vuedemo/BootStrap.groovy
@@ -1,8 +1,27 @@
 package vuedemo
 
+import demo.*
+import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+
+@Slf4j
 class BootStrap {
 
+    @Autowired
+    BookService bookService
+
+    @Autowired
+    AuthorService authorService
+
     def init = { servletContext ->
+        log.info "Populating database"
+        Author ken = authorService.save("Ken Kousen")
+
+        bookService.save "Making Java Groovy", 800, ken
+        bookService.save "Gradle Recipes", 500, ken
+        bookService.save "Android Recipies", 600, ken
+
+
     }
     def destroy = {
     }
